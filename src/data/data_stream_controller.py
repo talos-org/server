159287b149f46bf103b3aaa41fd5103ab9cf812c
zaidoon1 <zaidoonabdalhadi@gmail.com>
2018-11-10 03:34:58 -0500

    You are now able to subscribe to a stream as well as unsubscribe. Basic error handling has also been added

--- src/data/data_stream_controller.py
+++ src/data/data_stream_controller.py
@@ -1,4 +1,5 @@
 from plumbum import local, FG, BG, TF, RETCODE
+import json
 
 
 class DataStreamController:
@@ -17,18 +18,23 @@ class DataStreamController:
         self._get_stream_publisher_items = self._multichain['liststreampublisheritems']
         self._get_stream_publishers = self._multichain['liststreampublishers']
 
-    def create_stream(self, name, isOpen):
+    def create_stream(self, name: str, isOpen: bool):
         """
-        Creates a new stream on the blockchain called name. Pass the value "stream" in the type 
-        parameter (the create API can also be used to create upgrades). If open is true then 
-        anyone with global send permissions can publish to the stream, otherwise publishers 
-        must be explicitly granted per-stream write permissions. Returns the txid of the transaction creating the stream.
+        Creates a new stream on the blockchain called name. 
+        Pass the value "stream" in the type parameter. If open is true 
+        then anyone with global send permissions can publish to the stream, 
+        otherwise publishers must be explicitly granted per-stream write permissions. 
+        Returns the txid of the transaction creating the stream.
         """
-        txid = self._create_stream(name, str(isOpen))
-        # need to validate output to ensure that everything is ok
-        return txid
+        try:
+
+            txid = self._create_stream(name, str(isOpen))
+            # need to validate output to ensure that everything is ok
+            return txid
+        except Exception as err:
+            print(err)
     
-    def get_streams(self, verbose, names = None):
+    def get_streams(self, verbose: bool, names: list = None):
         """
         Returns information about streams created on the blockchain. Pass a stream name, ref or
         creation txid in streams to retrieve information about one stream only, an array thereof 
@@ -36,17 +42,59 @@ class DataStreamController:
         list only, with negative start values (like the default) indicating the most recently 
         created streams. Extra fields are shown for streams to which this node has subscribed.
         """
-        if names is not None:
-            streams = self._get_streams(names, str(verbose))
-        else:
-            streams = self._get_streams('*', str(verbose))
-        # need to validate output
-        return streams
+        try:
+            if names is not None:
+                streams = self._get_streams(str(names), str(verbose))
+            else:
+                streams = self._get_streams('*', str(verbose))
+            # need to validate output
+            return streams
+        except Exception as err:
+            print(err)
+        
     
-    def publish_item(self, stream_name, keys, data, data_type = 'json'):
+    def publish_item(self, stream_name: str, keys: list, data: str):
         """
-        Publishes an item in stream, passed as a stream name, ref or creation txid, 
-        with key provided in text form or an array of keys and data in text or JSON (set in the data type field).
+        Publishes an item in stream, passed as a stream name, an array of keys 
+        and data in JSON format.
         """
-        txid = self._publish_item(stream_name, str(keys), '{{"{0}":{1}}}'.format(data_type, data))
-        return txid
+        try:
+            json_data = json.loads(data)
+            formatted_data = json.dumps({"json": json_data})
+            txid = self._publish_item(stream_name, str(keys), formatted_data)
+            return txid
+        except ValueError as err:
+            print(err)
+        except Exception as err:
+            print(err)
+    
+    def subscribe(self, streams: list, rescan: bool = False):
+        """
+        Instructs the node to start tracking one or more stream(s). 
+        These are specified using an array of one or more items. 
+        If rescan is true, the node will reindex all items from when the streams 
+        were created, as well as those in other subscribed entities. 
+        Returns True if successful.
+        """
+        try:
+            output = self._subscribe_to_stream(str(streams), str(rescan))
+            if output == 'null':
+                return True
+            else:
+                return False
+        except Exception as err:
+            print(err)
+    
+    def unsubscribe(self, streams: list):
+        """
+        Instructs the node to stop tracking one or more stream(s). 
+        Streams are specified using an array of one ore more items.
+        """
+        try:
+            output = self._unsubscribe_from_stream(str(streams))
+            if output == 'null':
+                return True
+            else:
+                return False
+        except Exception as err:
+            print(err)

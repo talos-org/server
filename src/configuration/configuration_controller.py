96729bba82ce4ae6cc6b0fbbdbd872e9eb03fae5
darshgrewal <grewaldarshpreet@gmail.com>
2018-11-22 17:21:50 -0500

    Added configurable path for Node location and configurable path for Talos install. Cleaned up the config_params function

--- src/configuration/configuration_controller.py
+++ src/configuration/configuration_controller.py
@@ -1,20 +1,26 @@
-from plumbum import local, FG, BG, TF, RETCODE, ProcessExecutionError
-import subprocess
-
+from subprocess import run, CalledProcessError
+import os
+from configparser import RawConfigParser
+from configobj import ConfigObj
+from pathlib import Path
 
 class ConfiguartionController:
-    def __init__(self):
-        self._multichain_util = local['multichain-util']
-        self._multichain_d = 'multichaind'
+    def __init__(self, params_path='', install_path=''):
+        self._multichain_util_arg = ['./multichain-util']
+        self._multichain_d_arg = ['./multichaind']
+        self._create = self._multichain_util_arg+['create']
+        self._data_dir_arg = "-datadir="
         self._multichain_daemon = "-daemon"
-        self._create = self._multichain_util['create']
-        self._multichain_path = "~/.multichain/"
+        self._multichain_path = "/.multichain/"
         self._params_file = "/params.dat"
-        self._chain_description = "chain-description = "
-        self._max_block_size = "maximum-block-size = "
-        self._target_block_time = "target-block-time = "
-        self._mining_turnover = "mining-turnover = "
-        self._mining_diversity = "mining-diversity = "
+        self._chain_description = "chain-description"
+        self._max_block_size = "maximum-block-size"
+        self._target_block_time = "target-block-time"
+        self._mining_turnover = "mining-turnover"
+        self._mining_diversity = "mining-diversity"
+        self._dummy_section = 'dummy_section'
+        self._params_path = self.validate_params_path(params_path)
+        self._install_path = self.validate_install_path(install_path)
 
     def create_chain(self,blockchain_name: str):
         """
@@ -28,14 +34,16 @@ class ConfiguartionController:
         To generate blockchain please run "multichaind blockchain_name -daemon".
         """
         try:
-            output = self._create[blockchain_name].run(retcode=0)
-            return output[1].strip()
-        except ProcessExecutionError as err:
-            print(err.args[3])
+            cmd = self._create + [blockchain_name]+[self._data_dir_arg+self._params_path]
+            output = run(cmd, check=True, capture_output=True, cwd=self._install_path)
+            return output.stdout.strip()
+        except CalledProcessError as err:
+            print(err.stderr)
         except Exception as err:
             print(err)
 
-    def config_params(self, blockchain_name=None,description=None, max_block_size=None, target_block_time=None, mining_turnover=None, mining_diversity=None):
+    def config_params(self, blockchain_name: str, params_dict:{}):
+
         """
         The following parameters of a blockchain can be be configured after creating the chain but before starting the chain.
         The parameters are set in the params.dat file for each blockchain, and once the blockchain has been intialized these parameters can not be changed.
@@ -51,24 +59,12 @@ class ConfiguartionController:
         :return Confirmation messange acknowledging that the parameters have been successfully added to the params.data file:
         """
         try:
-            path = local[self._multichain_path+blockchain_name+self._params_file]
-            file = open(str(path), 'r')
-            lines = file.readlines()
-            file.close()
-            file2 = open(str(path), 'w')
-            for line in lines:
-                if (description is not None) & (self._chain_description in line):
-                    line = self._chain_description + description+"\n"
-                if (max_block_size is not None) & (self._max_block_size in line):
-                    line = self._max_block_size + max_block_size+"\n"
-                if (target_block_time is not None) & (self._target_block_time in line):
-                    line = self._target_block_time + target_block_time+"\n"
-                if (mining_turnover is not None) & (self._mining_turnover in line):
-                    line = self._mining_diversity + mining_turnover+"\n"
-                if (mining_diversity is not None) & (self._mining_diversity in line):
-                    line = self._mining_diversity + mining_diversity+"\n"
-                file2.write(line)
-            file2.close()
+            config = ConfigObj(self._params_path+blockchain_name + self._params_file)
+
+            for key in params_dict:
+                config[key]=params_dict.get(key)
+
+            config.write()
         except Exception as err:
             print(err)
 
@@ -80,9 +76,27 @@ class ConfiguartionController:
         """
 
         try:
-            cmd = [self._multichain_d,blockchain_name, self._multichain_daemon]
-            subprocess.run(cmd, timeout=5)
-        except ProcessExecutionError as err:
-            print(err.args[3])
+            cmd = self._multichain_d_arg+[blockchain_name, self._multichain_daemon]+[self._data_dir_arg+self._params_path]
+            run(cmd, timeout=5, cwd=self._install_path)
+        except CalledProcessError as err:
+            print(err.stderr)
         except Exception as err:
             print(err)
+
+    def validate_params_path(self,path):
+        """
+        Validates the the params path provided by the user
+        :return: a valid path of params.dat location
+        """
+        if os.path.exists(path):
+            return path
+        return str(Path.home())+self._multichain_path
+
+    def validate_install_path(self,path):
+        """
+        Validates the the isntall path provided by the user
+        :return: a valid path of install path of MultiChain
+        """
+        if os.path.exists(path):
+            return path
+        return '/usr/local/bin'#This is the default path of install which is generally used normal user programs that are not generally managed by the distribuation package manager
